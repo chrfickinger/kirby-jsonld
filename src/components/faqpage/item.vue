f0535096e31429a6f083d7ce827f7ec74211d5cb
Christian Fickinger <git@christianfickinger.de>
2021-06-14 09:29:13 +0200

    Update item.vue

--- src/components/faqpage/item.vue
+++ src/components/faqpage/item.vue
@@ -1,6 +1,6 @@
 <template>
 	<div @dblclick="open">
-		<strong contenteditable="true" @blur="update({ question: $event.target.innerText });">{{content.question}}</strong>
-		<div style="margin-top:.3em;" v-html="content.answer" contenteditable="true" @blur="update({ answer: $event.target.innerText });"></div>
+		<strong @blur="update({ question: $event.target.innerText });">{{content.question}}</strong>
+		<div style="margin-top:.3em;" v-html="content.answer" @blur="update({ answer: $event.target.innerText });"></div>
 	</div>
 </template>
